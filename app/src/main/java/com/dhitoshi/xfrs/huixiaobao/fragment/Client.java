fdf2bdcaeb182de8bb6431fdc089a3657ded72e1
dxs <dengxiaosong@66law.cn>
2017-09-07 17:54:51 +0800

    筛选点击效果

--- app/src/main/java/com/dhitoshi/xfrs/huixiaobao/fragment/Client.java
+++ app/src/main/java/com/dhitoshi/xfrs/huixiaobao/fragment/Client.java
@@ -1,5 +1,7 @@
 package com.dhitoshi.xfrs.huixiaobao.fragment;
+
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,8 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
-
+import android.widget.TextView;
 import com.dhitoshi.xfrs.huixiaobao.Bean.ClientBean;
 import com.dhitoshi.xfrs.huixiaobao.Bean.Menu;
 import com.dhitoshi.xfrs.huixiaobao.Bean.ScreenBean;
@@ -20,24 +21,30 @@ import com.dhitoshi.xfrs.huixiaobao.common.PopupMenu;
 import com.dhitoshi.xfrs.huixiaobao.presenter.ClientPresenter;
 import com.dhitoshi.xfrs.huixiaobao.view.AddClient;
 import com.dhitoshi.xfrs.huixiaobao.view.ClientInfo;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 //客户页面
-public class Client extends Fragment implements ClientManage.View{
+public class Client extends Fragment implements ClientManage.View {
     Unbinder unbinder;
     @BindView(R.id.client_menu)
     ImageView clientMenu;
+    @BindView(R.id.role_text)
+    TextView roleText;
+    @BindView(R.id.type_text)
+    TextView typeText;
+    @BindView(R.id.sort_text)
+    TextView sortText;
     private Drawable drawable;
+    private Drawable defualt;
     private Menu menu;
     private List<Menu> menus;
-    private  PopupMenu popupMenu;
+    private PopupMenu popupMenu;
     private Intent it;
+    private int screen_oldPosition=-1;
     public Client() {
     }
     public static Client newInstance() {
@@ -48,6 +55,7 @@ public class Client extends Fragment implements ClientManage.View{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_client, container, false);
@@ -56,8 +64,12 @@ public class Client extends Fragment implements ClientManage.View{
         return view;
     }
     private void initViews() {
-        ClientPresenter clientPresenter=new ClientPresenter(this);
+        ClientPresenter clientPresenter = new ClientPresenter(this);
         clientPresenter.getSelectCustomer();
+        defualt = getContext().getResources().getDrawable(R.mipmap.down);
+        defualt.setBounds(0, 0, defualt.getMinimumWidth(), defualt.getMinimumHeight());
+        drawable = getContext().getResources().getDrawable(R.mipmap.up);
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), drawable.getMinimumHeight());
     }
     @Override
     public void onDestroyView() {
@@ -71,24 +83,63 @@ public class Client extends Fragment implements ClientManage.View{
                 popupMenu();
                 break;
             case R.id.client_role:
+                screenRole();
                 break;
             case R.id.client_type:
+                screenRoleType();
                 break;
             case R.id.client_sort:
+                screenType();
                 break;
+        }
+    }
+    //筛选角色
+    private void screenRole() {
+        if(screen_oldPosition==0){
+            roleText.setCompoundDrawables(null,null,null,defualt);
+            roleText.setTextColor(Color.parseColor("#666666"));
+            screen_oldPosition=-1;
+        }else{
+            roleText.setCompoundDrawables(null,null,null,drawable);
+            roleText.setTextColor(Color.parseColor("#34B1FF"));
+            screen_oldPosition=0;
+        }
+    }
+    //筛选类型
+    private void screenRoleType() {
+        if(screen_oldPosition==1){
+            typeText.setCompoundDrawables(null,null,null,defualt);
+            typeText.setTextColor(Color.parseColor("#666666"));
+            screen_oldPosition=-1;
+        }else{
+            typeText.setCompoundDrawables(null,null,null,drawable);
+            typeText.setTextColor(Color.parseColor("#34B1FF"));
+            screen_oldPosition=1;
+        }
+    }
+    //筛选排序
+    private void screenType() {
+        if(screen_oldPosition==2){
+            sortText.setCompoundDrawables(null,null,null,defualt);
+            sortText.setTextColor(Color.parseColor("#666666"));
+            screen_oldPosition=-1;
+        }else{
+            sortText.setCompoundDrawables(null,null,null,drawable);
+            sortText.setTextColor(Color.parseColor("#34B1FF"));
+            screen_oldPosition=2;
         }
     }
     //弹出菜单
     private void popupMenu() {
-        if(null==popupMenu){
+        if (null == popupMenu) {
             initMenuData();
-            popupMenu=PopupMenu.Build(menus, getContext(),clientMenu).init();
+            popupMenu = PopupMenu.Build(menus, getContext(), clientMenu).init();
             initMenuClick(popupMenu);
             popupMenu.show();
-        }else{
-            if(!popupMenu.isShowing()){
+        } else {
+            if (!popupMenu.isShowing()) {
                 popupMenu.show();
-            }else{
+            } else {
                 popupMenu.dismisss();
             }
         }
@@ -98,15 +149,15 @@ public class Client extends Fragment implements ClientManage.View{
         popupMenu.addMenuItemClickListener(new MenuItemClick<Menu>() {
             @Override
             public void onMenuItemClick(Menu menu, int position) {
-                switch (position){
+                switch (position) {
                     case 0:
                         break;
                     case 1:
-                        it=new Intent(getContext(), AddClient.class);
+                        it = new Intent(getContext(), AddClient.class);
                         startActivity(it);
                         break;
                     case 2:
-                        it=new Intent(getContext(), ClientInfo.class);
+                        it = new Intent(getContext(), ClientInfo.class);
                         startActivity(it);
                         break;
                     case 3:
@@ -161,6 +212,6 @@ public class Client extends Fragment implements ClientManage.View{
     //获取筛选条件信息
     @Override
     public void getSelectCustomer(ScreenBean screenBean) {
-       // Log.e("TAG","客户类型数量:"+screenBean.getCustomer_type().size());
+         Log.e("TAG","客户类型数量:"+screenBean.getCustomer_type().size());
     }
 }
