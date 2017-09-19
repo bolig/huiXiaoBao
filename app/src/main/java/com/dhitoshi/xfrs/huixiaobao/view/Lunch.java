ed9bce281776938edf77931cdb30d2864fb0907a
dxs <dengxiaosong@66law.cn>
2017-09-19 18:18:46 +0800

    我的页面

---
+++ app/src/main/java/com/dhitoshi/xfrs/huixiaobao/view/Lunch.java
@@ -0,0 +1,12 @@
+package com.dhitoshi.xfrs.huixiaobao.view;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dhitoshi.xfrs.huixiaobao.R;
+public class Lunch extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lunch);
+    }
+}
