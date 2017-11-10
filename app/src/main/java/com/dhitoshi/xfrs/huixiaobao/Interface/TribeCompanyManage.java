d1e636c3cd55f01f6c6fc431f1aa3875e2fa4582
dxs <dengxiaosong@66law.cn>
2017-11-10 17:57:22 +0800

    我的页面

---
+++ app/src/main/java/com/dhitoshi/xfrs/huixiaobao/Interface/TribeCompanyManage.java
@@ -0,0 +1,19 @@
+package com.dhitoshi.xfrs.huixiaobao.Interface;
+import com.dhitoshi.refreshlayout.SmartRefreshLayout;
+import com.dhitoshi.xfrs.huixiaobao.Bean.BaseBean;
+import com.dhitoshi.xfrs.huixiaobao.Bean.HttpPageBeanTwo;
+/**
+ * Created by dxs on 2017/11/10.
+ */
+
+public class TribeCompanyManage {
+    interface View{
+        void getCompanyList(HttpPageBeanTwo<BaseBean> httpPageBeanTwo);
+    }
+    interface Model{
+        void getCompanyList(String token, String page, SmartRefreshLayout refreshLayout,Callback<HttpPageBeanTwo<BaseBean>> callback);
+    }
+    interface Presenter{
+        void getCompanyList(String token, String page, SmartRefreshLayout refreshLayout);
+    }
+}
